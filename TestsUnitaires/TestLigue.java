df84b54423adff49263643f57022c31d158a1a76
Samirsj <Fossard951400@gmail.com>
2024-11-03 17:43:17 +0100

    Ajout des testsUnitaires

---
+++ TestsUnitaires/TestLigue.java
@@ -0,0 +1,45 @@
+package testsUnitaires;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import personnel.*;
+
+class testLigue 
+{
+	GestionPersonnel gestionPersonnel = GestionPersonnel.getGestionPersonnel();
+
+	@Test
+	void createLigue() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		assertEquals("Fléchettes", ligue.getNom());
+	}
+
+ 
+	@Test
+	void getNom() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		assertEquals("Fléchettes", ligue.getNom());
+	}
+	
+	@Test
+	void setNom() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		ligue.setNom("Billard");
+		assertEquals("Billard", ligue.getNom());
+	}
+	
+	@Test
+	void getAdministrateur() throws SauvegardeImpossible
+	{
+		Ligue ligue = gestionPersonnel.addLigue("Fléchettes");
+		Employe employe = ligue.addEmploye("Bouchard", "Gerard", "g.bouchard@gmail.com", "azerty"); 
+		ligue.setAdministrateur(employe);
+		assertEquals("root", employe.getNom());
+	}
+
+}
