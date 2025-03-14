b7b11ffeca5bc06f4f03f321934fa990788621ce
ES SAFI Samir <sessafi@MDNPEDAGO.local>
2025-03-14 12:16:19 +0100

    Maj database

--- Itération 2/Création_Table.sql
+++ Itération 2/Création_Table.sql
@@ -1,22 +1,22 @@
 
+CREATE TABLE `Ligue` (
+    `ID_Ligue` int NOT NULL AUTO_INCREMENT,
+    `Nom_Ligue` varchar(50) DEFAULT NULL,
+    PRIMARY KEY (`ID_Ligue`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE TABLE Ligue (
-    ID_Ligue INT ,
-    Nom_Ligue VARCHAR(50),
-    PRIMARY KEY(ID_Ligue)
-);
 
-
-CREATE TABLE Employé (
-    ID_Employé INT ,
-    Nom_Employé VARCHAR(20),
-    Prenom_Employé VARCHAR(20),
-    Mail_Employé VARCHAR(50),
-    MDP_Employé VARCHAR(50),
-    Fonction_Employé VARCHAR(50),
-    Date_Depart DATE,
-    Date_Arrivee DATE,
-    ID_Ligue INT,
-    PRIMARY KEY(ID_Employé),
-    FOREIGN KEY(ID_Ligue) REFERENCES Ligue(ID_Ligue)
-);
+CREATE TABLE `Employe` (
+    `ID_Employé` int NOT NULL AUTO_INCREMENT,
+    `Nom_Employé` varchar(20) DEFAULT NULL,
+    `Prenom_Employé` varchar(20) DEFAULT NULL,
+    `Mail_Employé` varchar(50) DEFAULT NULL,
+    `MDP_Employé` varchar(50) DEFAULT NULL,
+    `Fonction_Employe` varchar(50) DEFAULT NULL,
+    `Date_Depart` date DEFAULT NULL,
+    `Date_Arrivee` date DEFAULT NULL,
+    `ID_Ligue` int DEFAULT NULL,
+    PRIMARY KEY (`ID_Employe`),
+    KEY `fk_ligue` (`ID_Ligue`),
+    CONSTRAINT `fk_ligue` FOREIGN KEY (`ID_Ligue`) REFERENCES `ligue` (`ID_Ligue`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
